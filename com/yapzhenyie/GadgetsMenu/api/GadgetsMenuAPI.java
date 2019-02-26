1866527b77ef9eca1c4576f11b592b4d959beefe
Yap Zhen Yie <OnlyNoobDied@users.noreply.github.com>
2019-02-26 17:53:56 +0800

    Improve developer API.

---
+++ com/yapzhenyie/GadgetsMenu/api/GadgetsMenuAPI.java
@@ -0,0 +1,24 @@
+package com.yapzhenyie.GadgetsMenu.api;
+
+import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
+
+import com.yapzhenyie.GadgetsMenu.GadgetsMenu;
+import com.yapzhenyie.GadgetsMenu.player.OfflinePlayerManager;
+import com.yapzhenyie.GadgetsMenu.player.PlayerManager;
+
+public class GadgetsMenuAPI {
+
+	public static PlayerManager getPlayerManager(Player player) {
+		return GadgetsMenu.getPlayerManager(player);
+	}
+
+	public static OfflinePlayerManager getOfflinePlayerManager(OfflinePlayer offlinePlayer) {
+		return new OfflinePlayerManager(offlinePlayer);
+	}
+
+	public static void goBackToMainMenu(Player player) {
+		GadgetsMenu.getPlayerManager(player).goBackToMainMenu();
+	}
+
+}
